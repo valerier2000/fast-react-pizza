671eedfeff030f349dbd0f5e90c698010b005971
Valeriia Rohatynska <valeriiarohatynska@gmail.com>
2024-05-02 17:13:19 +0200

    Add functionality to delete Cart items

--- src/features/menu/MenuItem.jsx
+++ src/features/menu/MenuItem.jsx
@@ -1,12 +1,16 @@
 /* eslint-disable react/prop-types */
 import { formatCurrency } from "../../utils/helpers";
 import Button from "../../ui/Button";
-import { useDispatch } from "react-redux";
-import { addItem } from "../cart/cartSlice.js";
+import { useDispatch, useSelector } from "react-redux";
+import { addItem, getCurrentQuantityById } from "../cart/cartSlice.js";
+import DeleteItem from "../cart/DeleteItem.jsx";
 
 function MenuItem({ pizza }) {
   const dispatch = useDispatch();
   const { id, name, unitPrice, ingredients, soldOut, imageUrl } = pizza;
+
+  const currentQuantity = useSelector(getCurrentQuantityById(id));
+  const isnInCart = currentQuantity > 0;
 
   function handleAddToCart() {
     const newItem = {
@@ -40,7 +44,9 @@ function MenuItem({ pizza }) {
             </p>
           )}
 
-          {!soldOut && (
+          {isnInCart && <DeleteItem pizzaId={id} />}
+
+          {!soldOut && !isnInCart && (
             <Button type="small" onClick={handleAddToCart}>
               Add to cart
             </Button>
