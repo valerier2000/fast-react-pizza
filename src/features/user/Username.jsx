f79f72c88d4af682345fe0e7ec090cfcf9749026
Valeriia Rohatynska <valeriiarohatynska@gmail.com>
2024-05-02 08:22:31 +0200

    Add spacing, border, display properties

--- src/features/user/Username.jsx
+++ src/features/user/Username.jsx
@@ -1,5 +1,5 @@
 function Username() {
-  return <div className="text-sm font-semibold">Valerie</div>;
+  return <div className="hidden text-sm font-semibold">Valerie</div>;
 }
 
 export default Username;
