41584b898a1b84e1f996603f3c080e43b39d02a7
Valeriia Rohatynska <valeriiarohatynska@gmail.com>
2024-05-02 08:46:35 +0200

    Add flexbox

--- src/features/user/Username.jsx
+++ src/features/user/Username.jsx
@@ -1,5 +1,5 @@
 function Username() {
-  return <div className="hidden text-sm font-semibold">Valerie</div>;
+  return <div className="hidden text-sm font-semibold md:block">Valerie</div>;
 }
 
 export default Username;
