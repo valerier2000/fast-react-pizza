c6fd5611f81576262a680e96f1753bdc793c3c71
Valeriia Rohatynska <valeriiarohatynska@gmail.com>
2024-08-16 08:01:45 +0200

    Add UpdateOrder component to handle order prioritization after the order was made

--- src/features/order/Order.jsx
+++ src/features/order/Order.jsx
@@ -9,6 +9,7 @@ import {
 } from "../../utils/helpers";
 import OrderItem from "../order/OrderItem";
 import { useEffect } from "react";
+import UpdateOrder from "./UpdateOrder";
 
 function Order() {
   const order = useLoaderData();
@@ -86,6 +87,8 @@ function Order() {
           To pay on delivery: {formatCurrency(orderPrice + priorityPrice)}
         </p>
       </div>
+
+      {!priority && <UpdateOrder order={order} />}
     </div>
   );
 }
